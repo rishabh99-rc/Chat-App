47ab3a8aff659f257bd855d2ac46c985e4c8273a
Rishabh Chauhan <binareshu96@gmail.com>
2022-08-25 01:19:31 +0530

    Create firebase.init.js

---
+++ ChatApp/scripts/firebase.init.js
@@ -0,0 +1,9 @@
+const firebaseApp = firebase.initializeApp({ 
+    apiKey: "AIzaSyDazQbEbW_2Ow-05idvg083izNPDaOdfwk",
+    authDomain: "chat-app-a246e.firebaseapp.com",
+     projectId: "chat-app-a246e",
+    storageBucket: "chat-app-a246e.appspot.com",
+    messagingSenderId: "847045701787",
+    appId: "1:847045701787:web:152563baa3c7267538163e"
+    });
+   const db = firebaseApp.firestore();
